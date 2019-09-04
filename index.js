101e0926d313df667d597d825dd79dad97811428
Garvan Doyle <garvandoyle@gmail.com>
2019-09-03 23:07:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+$('#videoLink')
+.magnificPopup({
+	  type:'inline',
+	  midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+  })
+
+
 $(document).ready(function(){
   $("a").on('click', function(event) {
     if (this.hash !== "") {
